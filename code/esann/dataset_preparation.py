4a4ac4e5fb244f9065a9c701e5eafab0ff1e267d
Sérgio Filho <sergio.vasconcelos@einstein.br>
2024-04-22 19:06:54 -0300

    refactor Movie Review dataset download

--- code/esann/dataset_preparation.py
+++ code/esann/dataset_preparation.py
@@ -8,24 +8,40 @@ class DatasetProcessor:
     def __init__(self):
         self._path = get_path()
 
-class MovieReviewProcessor(DatasetProcessor):
+    def download(self):
+        raise NotImplementedError()
+
     def load(self):
-        url = "https://www.cs.cornell.edu/people/pabo/movie-review-data/review_polarity.tar.gz"
-        root_path = self._path["data"] / "movie_review"
-        if not root_path.exists():
-            root_path.mkdir(parents=True, exist_ok=True)
-        file_path_compressed = root_path / "review_polarity.tar.gz"
-        folder_path_uncompressed = root_path / "txt_sentoken"
-        if not file_path_compressed.exists():
+        raise NotImplementedError()
+
+    def create_embeddings(self):
+        raise NotImplementedError()
+
+class MovieReviewProcessor(DatasetProcessor):
+    _url = "https://www.cs.cornell.edu/people/pabo/movie-review-data/review_polarity.tar.gz"
+    def __init__(self):
+        super().__init__()
+        self.df = None
+        self._path["root_dataset_folder"] = self._path["data"] / "movie_review"
+        if not self._path["root_dataset_folder"].exists():
+            self._path["root_dataset_folder"].mkdir(parents=True, exist_ok=True)
+
+        self._path["file_path_compressed"] = self._path["root_dataset_folder"] / "review_polarity.tar.gz"
+        self._path["folder_path_uncompressed"] = self._path["root_dataset_folder"] / "review_polarity"
+
+    def download(self):
+        if not self._path["file_path_compressed"].exists():
             # From https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
-            with urllib.request.urlopen(url) as response, open(file_path_compressed, 'wb') as file:
+            with urllib.request.urlopen(self._url) as response, open(self._path["file_path_compressed"], 'wb') as file:
                 shutil.copyfileobj(response, file)
 
-        if not folder_path_uncompressed.exists():
-            with tarfile.open(file_path_compressed, "r:gz") as tar:
-                tar.extractall(root_path / "review_polarity")
+        if not self._path["folder_path_uncompressed"].exists():
+            with tarfile.open(self._path["file_path_compressed"], "r:gz") as tar:
+                tar.extractall(self._path["folder_path_uncompressed"])
 
 
+    def load(self):
+        pass
 
 class DatasetPreparation:
     _options = {"MR": MovieReviewProcessor}
@@ -34,4 +50,6 @@ class DatasetPreparation:
 
     def load(self, dataset_initials: str):
         df_processor = self._options[dataset_initials]()
+        df_processor.download()
         df_processor.load()
+        df_processor.create_embeddings()
