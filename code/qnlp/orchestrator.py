0dc3fe1ac7e3316bbc65a4002e2290c28da33982
Sérgio Filho <sergio.vasconcelos@einstein.br>
2024-08-08 14:57:48 -0300

    aggregate final data

--- code/qnlp/orchestrator.py
+++ code/qnlp/orchestrator.py
@@ -1,8 +1,10 @@
 import logging
 import random
+import shutil
 from pathlib import Path
 
 import numpy as np
+import pandas as pd
 import torch
 from joblib import Parallel, delayed
 from pytreebank import load_sst
@@ -21,19 +23,22 @@ def transform_binary_label(label):
 class QNLP:
     def __init__(self, testing=False):
         torch.use_deterministic_algorithms(True)
+        torch.backends.cudnn.benchmark = False
         torch.manual_seed(42)
         random.seed(42)
         np.random.seed(42)
 
         self.testing = testing
         self._path = None
-        self._df = None
-        self._repetitions = 30
+
+        if self.testing:
+            self._repetitions = 3
+        else:
+            self._repetitions = 30
         self._define_log()
         self._define_path()
 
     def run(self):
-        self._get_data()
         self._process_data()
         self._run_models()
         self._agg_metrics()
@@ -45,28 +50,30 @@ class QNLP:
             format="%(asctime)s [%(levelname)s] %(message)s",
             handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
         )
-        logging.basicConfig(filename="", encoding="utf-8", level=logging.DEBUG)
 
     def _define_path(self):
         self._path = {"root": Path(__file__).parent.parent.parent.resolve()}
         self._path["data"] = self._path["root"] / "data"
 
-    def _get_data(self):
-        logging.info("Getting data.")
-        self._df = load_sst(self._path["data"] / "sst")
-
     def _process_data(self):
+        logging.info("Getting data.")
+        df = load_sst(self._path["data"] / "sst")
+
         logging.info("Processing data.")
         self._path["sst_processed"] = self._path["data"] / "sst_processed"
+
         device = "cuda" if torch.cuda.is_available() else "cpu"
         tokenizer = BertTokenizer.from_pretrained(
             "bert-large-uncased", use_fast=True, device=device
         )
         bert = BertModel.from_pretrained("bert-large-uncased").to(device)
+
         for seed in range(self._repetitions):
-            for dataset_name in self._df:
-                dataset = self._df[dataset_name]
-                dataset_path = self._path["sst_processed"] / dataset_name
+            for dataset_name in df:
+                dataset = df[dataset_name]
+                dataset_path = self._path["sst_processed"] / str(seed) / dataset_name
+                if (dataset_path / "values.pth").exists():
+                    continue
                 dataset_path.mkdir(parents=True, exist_ok=True)
                 for i, sentence in enumerate(dataset):
                     sentence_path = dataset_path / f"{i}.pth"
@@ -87,6 +94,7 @@ class QNLP:
                     embedding = self._get_embedding(tokenizer, bert, device, sentence)
                     self._save_embedding(sentence_path, sentence, label, embedding)
                     self._save_state()
+                self._agg_dataset(dataset_path)
 
     def _get_embedding(self, tokenizer, bert, device, sentence):
         sentence_tokenized = tokenizer(
@@ -128,6 +136,26 @@ class QNLP:
         torch.cuda.set_rng_state(states["gpu_rng_state"])
         np.random.set_state(states["numpy_rng_state"])
         random.setstate(states["py_rng_state"])
+
+    def _agg_dataset(self, dataset_path):
+        sentences = dataset_path.glob("*.pth")
+        sentences = pd.DataFrame([torch.load(sentence) for sentence in sentences])
+        to_delete = sentences[sentences["label"] == -1]
+        logging.info(
+            "Deleting %d sentences in %s dataset.",
+            len(to_delete),
+            dataset_path.parts[-1],
+        )
+        sentences = sentences.drop(to_delete.index)
+        labels = sentences["label"].values
+        embeddings = np.row_stack(sentences["embedding"].values)
+        values = {
+            "embeddings": embeddings,
+            "labels": labels,
+        }
+        shutil.rmtree(dataset_path)
+        dataset_path.mkdir()
+        torch.save(values, dataset_path / "values.pth")
 
     def _run_models(self):
         pass
