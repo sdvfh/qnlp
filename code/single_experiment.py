c5e8af21c12ae0316cbb16a73a1ea15d53aaf4f1
Sérgio Filho <sergio.vasconcelos@einstein.br>
2024-11-17 22:03:11 -0300

    chore(experiment): eval single circuit

--- code/single_experiment.py
+++ code/single_experiment.py
@@ -1,11 +1,12 @@
 # Import libraries
+import numpy as np
 import numpy.random as npr
 from lambeq import AtomicType, BobcatParser, StronglyEntanglingAnsatz
 from lambeq.backend.numerical_backend import set_backend
 from sympy import default_sort_key
 
 # Set backend for lambeq
-set_backend("jax")
+set_backend("numpy")
 
 # Set numpy seed
 npr.seed(0)
@@ -32,6 +33,23 @@ vocab = sorted(circuit.free_symbols, key=default_sort_key)
 print("Vocabulary: ", vocab)
 
 # Add angles to vocabulary
-values_angles = [(npr.uniform(low=0.0, high=1),) for symbol in vocab]
+values_angles = [npr.uniform(low=0.0, high=1) for symbol in vocab]
 dict_angles = dict(zip(vocab, values_angles, strict=True))
 print("Vocabulary with angles: ", dict_angles)
+
+# Lambeq to PennyLane circuit
+qml_circ = circuit.to_pennylane(
+    probabilities=False,
+    backend_config=None,
+    diff_method="best",
+)
+
+# Initialise device and circuit
+qml_circ.initialise_device_and_circuit()
+qml_circ.initialise_concrete_params(dict_angles)
+
+# Draw circuit
+qml_circ.draw()
+
+# Eval circuit
+qml_circ.eval()
