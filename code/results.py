c56c85d5f192ed664bf59cbaeee68f36830d90c6
Sérgio de Vasconcelos Filho <sergio.vasconcelos@einstein.br>
2025-02-24 10:39:00 -0300

    add creation of figures

--- code/results.py
+++ code/results.py
@@ -1,5 +1,8 @@
+import csv
 import pickle
 from pathlib import Path
+
+import pandas as pd
 
 root_path: Path = Path(__file__).parent.parent.resolve()
 data_path: Path = root_path / "data"
@@ -15,3 +18,28 @@ for file in file_results_path:
         metrics = pickle.load(f)
         metrics["file"] = file.stem
         results.append(metrics)
+
+results = pd.DataFrame(results)
+results["model"] = results["file"].str.split("_").str[0]
+print("Hi")
+metrics = results.groupby(["n_layers", "model", "level"]).agg(
+    {
+        "accuracy": ["mean", "std"],
+        "f1": ["mean", "std"],
+        "precision": ["mean", "std"],
+        "recall": ["mean", "std"],
+    }
+)
+metrics.to_csv(
+    results_path / "metrics.csv",
+    index=True,
+    encoding="utf-8",
+    quoting=csv.QUOTE_MINIMAL,
+)
+
+results.to_csv(
+    results_path / "results.csv",
+    index=True,
+    encoding="utf-8",
+    quoting=csv.QUOTE_MINIMAL,
+)
