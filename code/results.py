a2878e6b65134613ae87d82db8838f0e05a40f0a
Sérgio de Vasconcelos Filho <sergio.vasconcelos@einstein.br>
2025-03-01 15:28:14 -0300

    update results folder in results.py

--- code/results.py
+++ code/results.py
@@ -7,7 +7,10 @@ import pandas as pd
 root_path: Path = Path(__file__).parent.parent.resolve()
 data_path: Path = root_path / "data"
 results_path: Path = root_path / "results"
-
+exp_name = "mpnet"
+n_qubits = 10
+truncate_dim = 32
+dir_path = results_path / exp_name / str(n_qubits) / str(truncate_dim)
 
 file_results_path = results_path.rglob("*.pkl")
 
@@ -21,7 +24,7 @@ for file in file_results_path:
 
 results = pd.DataFrame(results)
 results["model"] = results["file"].str.split("_").str[0]
-print("Hi")
+
 metrics = results.groupby(["n_layers", "model", "level"]).agg(
     {
         "accuracy": ["mean", "std"],
@@ -31,14 +34,7 @@ metrics = results.groupby(["n_layers", "model", "level"]).agg(
     }
 )
 metrics.to_csv(
-    results_path / "metrics.csv",
-    index=True,
-    encoding="utf-8",
-    quoting=csv.QUOTE_MINIMAL,
-)
-
-results.to_csv(
-    results_path / "results.csv",
+    dir_path / "metrics.csv",
     index=True,
     encoding="utf-8",
     quoting=csv.QUOTE_MINIMAL,
