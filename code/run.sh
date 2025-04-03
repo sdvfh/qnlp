d50423d010825c489e1f368a4ca40d685f78d544
Sérgio de Vasconcelos Filho <sergio.vasconcelos@einstein.br>
2025-04-03 15:09:22 -0300

    add classical models to run

--- code/run.sh
+++ code/run.sh
@@ -31,3 +31,20 @@ for dataset in chatgpt_easy chatgpt_medium chatgpt_hard; do
     done
   done
 done
+
+for dataset in chatgpt_easy chatgpt_medium chatgpt_hard; do
+  for classifier in svmrbf svmlinear svmpoly logistic randomforest knn mlp; do
+    for layers in 1; do
+      # Block 1: Transformer all-mpnet-base-v2, n_features 768, n_qubits 10
+      python experiments.py -dataset "$dataset" -model_transformer "all-mpnet-base-v2" -n_features 768 -model_classifier "$classifier" --epochs 100 --batch_size 5 --n_repetitions 30 --n_layers "$layers" --n_qubits 10; \
+      # Block 2: Transformer all-mpnet-base-v2, n_features 32, n_qubits 10
+      python experiments.py -dataset "$dataset" -model_transformer "all-mpnet-base-v2" -n_features 32 -model_classifier "$classifier" --epochs 100 --batch_size 5 --n_repetitions 30 --n_layers "$layers" --n_qubits 10; \
+      # Block 3: Transformer all-mpnet-base-v2, n_features 32, n_qubits 5
+      python experiments.py -dataset "$dataset" -model_transformer "all-mpnet-base-v2" -n_features 32 -model_classifier "$classifier" --epochs 100 --batch_size 5 --n_repetitions 30 --n_layers "$layers" --n_qubits 5; \
+      # Block 4: Transformer tomaarsen/mpnet-base-nli-matryoshka, n_features 32, n_qubits 5
+      python experiments.py -dataset "$dataset" -model_transformer "tomaarsen/mpnet-base-nli-matryoshka" -n_features 32 -model_classifier "$classifier" --epochs 100 --batch_size 5 --n_repetitions 30 --n_layers "$layers" --n_qubits 5; \
+      # Block 5: Transformer nomic-ai/nomic-embed-text-v1.5, n_features 32, n_qubits 5
+      python experiments.py -dataset "$dataset" -model_transformer "nomic-ai/nomic-embed-text-v1.5" -n_features 32 -model_classifier "$classifier" --epochs 100 --batch_size 5 --n_repetitions 30 --n_layers "$layers" --n_qubits 5
+    done
+  done
+done
